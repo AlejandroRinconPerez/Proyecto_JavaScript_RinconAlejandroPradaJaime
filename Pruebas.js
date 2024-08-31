27f61ef3041789afbac1242f2d6ad1f96b062112
AlejandroRinconPerez <alejandro.ingenieria.estudios@gmail.com>
2024-08-31 09:10:18 -0500

    feat: :sparkles: Nuevo diseno en la barra de scroll para mejorar la apariencia de la pagina web
    Colores azules en la scroll bar
    
    @AlejandroRincon

--- Pruebas.js
+++ Pruebas.js
@@ -1,15 +1,15 @@
-// const showMenu = (toggleId, navId) =>{
-//     const toggle = document.getElementById(toggleId),
-//           nav = document.getElementById(navId)
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+          nav = document.getElementById(navId)
  
-//     toggle.addEventListener('click', () =>{
-//         nav.classList.toggle('show-menu')
+    toggle.addEventListener('click', () =>{
+        nav.classList.toggle('show-menu')
  
-//         toggle.classList.toggle('show-icon')
-//     })
-//  }
+        toggle.classList.toggle('show-icon')
+    })
+ }
  
-//  showMenu('nav-toggle','nav-menu')
+ showMenu('nav-toggle','nav-menu')
  
  let container =document.querySelector("#Contenedor")
  let url = "https://swapi.py4e.com/api/people/?page=";
